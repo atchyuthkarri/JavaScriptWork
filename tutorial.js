a04b15a77da24e0d1cbeb05ee232b249ff525583
atchyuthkarri <atchyuthkarri46@gmail.com>
2026-01-18 18:25:13 +0530

    Day 1: JS basics and closures

---
+++ tutorial.js
@@ -0,0 +1,23 @@
+
+
+a="apple";
+console.log(a);
+
+
+// var--- redefined and value changeable
+var bunty="playing"
+console.log(bunty);
+
+var bunty="running"
+console.log(bunty);
+
+//let--- not redinable but value changeable
+let school="ABC"
+console.log(school);
+
+school="primary"
+console.log(school);
+
+//let--- not redinable and not value changeable
+const ground="stadium"
+console.log(ground);
